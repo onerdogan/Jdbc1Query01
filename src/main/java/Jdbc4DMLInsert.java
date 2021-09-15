a435a50a9b99ea733494bcafcd18c0f8c29c1b9d
adınız <oner@gmail.com>
2021-09-15 23:54:25 +0300

    ver1 ver2 ver3 ve4 ver5

---
+++ src/main/java/Jdbc4DMLInsert.java
@@ -0,0 +1,77 @@
+import javax.swing.*;
+import java.sql.*;
+
+public class Jdbc4DMLInsert {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        Class.forName("oracle.jdbc.driver.OracleDriver");
+
+        Connection con=	DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","hr","hr");
+
+        Statement st=	con.createStatement();
+
+      ResultSet rs=st.executeQuery("select * from bolumler");
+        while (rs.next()){
+            System.out.println(rs.getInt(1)+rs.getString(2)+rs.getString(3));
+        }
+
+
+
+        //----------ORNEK1: Bolumler tablosuna yeni bir kayit (80, 'ARGE', 'ISTANBUL')
+        //ekleyelim ve eklenen kaydi teyit icin sorgulayalim.
+
+       // st.execute("insert into bolumler values(80, 'ARGE','ISTANBUL')");
+
+
+
+        //-------ORNEK2: Bolumler tablosuna birden fazla yeni kayıt ekleyelim.
+        // 1.YONTEM
+// -----------------------------------------------
+// Ayri ayri sorgular ile veritabanina tekrar tekrar ulasmak islemlerin
+// yavas yapilmasina yol acar. 10000 tane veri kaydi yapildigi dusunuldugunde
+        // bu kotu bir yaklasimdir.
+
+/*        String[] sorgular={"INSERT INTO bolumler VALUES(95, 'YEMEKHANE', 'ISTANBUL')",
+                           "INSERT INTO bolumler VALUES(85, 'OFIS','ANKARA')",
+                            "INSERT INTO bolumler VALUES(75, 'OFIS2', 'VAN')"};
+
+        for(String w:sorgular){
+            st.execute(w);
+        }*/
+
+
+// 2.YONTEM (addBath ve excuteBatch() metotlari ile)
+// ----------------------------------------------------
+// addBatch metodu ile SQL ifadeleri gruplandirilabilir ve executeBatch()
+// metodu ile veritabanina bir kere gonderilebilir.
+// excuteBatch() metodu bir int [] dizi dondurur. Bu dizi her bir ifade sonucunda
+// etkilenen satir sayisini gosterir.
+
+
+      /*  String [] sorgular1 = {"INSERT INTO bolumler VALUES(81, 'YEMEKHANE2', 'MUS')",
+                                "INSERT INTO bolumler VALUES(82, 'OFIS3','ORDU')",
+                                "INSERT INTO bolumler VALUES(83, 'OFIS4', 'MUGLA')"};
+
+        for(String w:sorgular1){
+            st.addBatch(w);
+        }
+        st.executeBatch();*/
+
+
+
+
+
+        // 3. YONTEM
+        //-----------------------------------------------------
+        // batch metoduyla birlikte PreparedStatement kullanmak en efektif yontemdir.
+        // bir sonraki ornekte bunu gerceklestirecegiz.
+
+
+
+
+
+
+        con.close();
+        st.close();
+        rs.close();
+    }
+}
